f0c27ba52177ef86551cf99446fdd0b475646528
Silverio Marín <73847643+smarinwm@users.noreply.github.com>
2022-06-14 13:28:00 +0200

    codificación EstructuraRepetitivaWhile

--- EstructuraRepetitivaWhile/EstructuraRepetitivaWhile/Program.cs
+++ EstructuraRepetitivaWhile/EstructuraRepetitivaWhile/Program.cs
@@ -10,6 +10,28 @@ namespace EstructuraRepetitivaWhile
     {
         static void Main(string[] args)
         {
+            int x, cantidad, n;
+            float largo;
+            string linea;
+            x = 1;
+            cantidad = 0;
+            Console.Write("Cuantas piezar procesará:");
+            linea = Console.ReadLine();
+            n = int.Parse(linea);
+            while (x <= n)
+            {
+                Console.Write("Ingrese la medida de la pieza:");
+                linea = Console.ReadLine();
+                largo = float.Parse(linea);
+                if (largo >= 1.20 && largo <= 1.30)
+                {
+                    cantidad = cantidad + 1;
+                }
+                x = x + 1;
+            }
+            Console.Write("La cantidad de piezas aptas son:");
+            Console.Write(cantidad);
+            Console.ReadKey();
         }
     }
 }
