dbfcdf64035f4076bd54823f263636eec25037fb
Silverio Marín <73847643+smarinwm@users.noreply.github.com>
2022-06-14 13:20:42 +0200

    codificación NotasAlumnos

--- NotasAlumno/NotasAlumno/Program.cs
+++ NotasAlumno/NotasAlumno/Program.cs
@@ -10,6 +10,34 @@ namespace NotasAlumno
     {
         static void Main(string[] args)
         {
+            int nota1, nota2, nota3;
+            string linea;
+            Console.Write("Ingrese primer nota:");
+            linea = Console.ReadLine();
+            nota1 = int.Parse(linea);
+            Console.Write("Ingrese segunda nota:");
+            linea = Console.ReadLine();
+            nota2 = int.Parse(linea);
+            Console.Write("Ingrese tercer nota:");
+            linea = Console.ReadLine();
+            nota3 = int.Parse(linea);
+            int promedio = (nota1 + nota2 + nota3) / 3;
+            if (promedio >= 9)
+            {
+                Console.Write("Sobresaliente");
+            }
+            else
+            {
+                if (promedio >= 5)
+                {
+                    Console.Write("Aprovado");
+                }
+                else
+                {
+                    Console.Write("Suspendido");
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
