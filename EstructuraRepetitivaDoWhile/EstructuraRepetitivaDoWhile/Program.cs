71271c4aaeb2b128d42e0bd925d586c7acd69c01
SilverioUserSvrDev <SilverioUserSvrDev@WINDEV2204EVAL>
2022-06-14 16:41:55 +0200

    codificación EstructuraRepetitivaDoWhile

--- EstructuraRepetitivaDoWhile/EstructuraRepetitivaDoWhile/Program.cs
+++ EstructuraRepetitivaDoWhile/EstructuraRepetitivaDoWhile/Program.cs
@@ -10,6 +10,46 @@ namespace EstructuraRepetitivaDoWhile
     {
         static void Main(string[] args)
         {
+            int cant1, cant2, cant3, suma;
+            float peso;
+            string linea;
+            cant1 = 0;
+            cant2 = 0;
+            cant3 = 0;
+            do
+            {
+                Console.Write("Ingrese el peso de la pieza (0 pera finalizar):");
+                linea = Console.ReadLine();
+                peso = float.Parse(linea);
+                if (peso > 10.2)
+                {
+                    cant1++;
+                }
+                else
+                {
+                    if (peso >= 9.8)
+                    {
+                        cant2++;
+                    }
+                    else
+                    {
+                        if (peso > 0)
+                        {
+                            cant3++;
+                        }
+                    }
+                }
+            } while (peso != 0);
+            suma = cant1 + cant2 + cant3;
+            Console.Write("Piezas aptas:");
+            Console.WriteLine(cant2);
+            Console.Write("Piezas con un peso superior a 10.2:");
+            Console.WriteLine(cant1);
+            Console.Write("Piezas con un peso inferior a 9.8:");
+            Console.WriteLine(cant3);
+            Console.Write("Cantidad de piezas procesadas:");
+            Console.WriteLine(suma);
+            Console.ReadLine();
         }
     }
 }
