04bd800e7c2e33b20e467d317c59ae1d0b0dc5fa
Silverio Marín <73847643+smarinwm@users.noreply.github.com>
2022-06-14 13:08:50 +0200

    codificación

--- CostoCompra/CostoCompra/Program.cs
+++ CostoCompra/CostoCompra/Program.cs
@@ -10,6 +10,19 @@ namespace CostoCompra
     {
         static void Main(string[] args)
         {
+            int cantidad;
+            float precio, importe;
+            string linea;
+            Console.Write("Ingrese la cantidad de artículos a llevar:");
+            linea = Console.ReadLine();
+            cantidad = int.Parse(linea);
+            Console.Write("Ingrese el valor unitario del producto:");
+            linea = Console.ReadLine();
+            precio = float.Parse(linea);
+            importe = precio * cantidad;
+            Console.Write("El importe total a pagar es:");
+            Console.Write(importe);
+            Console.ReadKey();
         }
     }
 }
