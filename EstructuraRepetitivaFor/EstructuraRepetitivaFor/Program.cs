2958b81cf08eea2389ed15fe98030b427ff9e66d
SilverioUserSvrDev <SilverioUserSvrDev@WINDEV2204EVAL>
2022-06-14 16:30:19 +0200

    codificación EstructuraRepetitivaFor

--- EstructuraRepetitivaFor/EstructuraRepetitivaFor/Program.cs
+++ EstructuraRepetitivaFor/EstructuraRepetitivaFor/Program.cs
@@ -10,6 +10,25 @@ namespace EstructuraRepetitivaFor
     {
         static void Main(string[] args)
         {
+            int cantidad, n, f, valor;
+            string linea;
+            cantidad = 0;
+            Console.Write("Cuantos valores ingresará:");
+            linea = Console.ReadLine();
+            n = int.Parse(linea);
+            for (f = 1; f <= n; f++)
+            {
+                Console.Write("Ingrese el valor:");
+                linea = Console.ReadLine();
+                valor = int.Parse(linea);
+                if (valor >= 1000)
+                {
+                    cantidad++;
+                }
+            }
+            Console.Write("La cantidad de valores ingresados mayores o iguales a 1000 son:");
+            Console.Write(cantidad);
+            Console.ReadKey();
         }
     }
 }
