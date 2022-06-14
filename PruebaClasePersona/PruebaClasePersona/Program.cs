efb3d71356541e8b3ef4ca829e8bdf7aef24847b
Silverio Marín <73847643+smarinwm@users.noreply.github.com>
2022-06-14 13:39:34 +0200

    class Persona y metodos

--- PruebaClasePersona/PruebaClasePersona/Program.cs
+++ PruebaClasePersona/PruebaClasePersona/Program.cs
@@ -8,7 +8,44 @@ namespace PruebaClasePersona
 {
     internal class Program
     {
-        static void Main(string[] args)
+        class Persona
+        {
+            private string nombre;
+            private int edad;
+
+            public void instanciar()
+            {
+                Console.Write("Ingrese el nombre:");
+                nombre = Console.ReadLine();
+                string linea;
+                Console.Write("Ingrese la edad:");
+                linea = Console.ReadLine();
+                edad = int.Parse(linea);
+            }
+
+            public void imprimir()
+            {
+                Console.Write("Nombre:");
+                Console.WriteLine(nombre);
+                Console.Write("Edad:");
+                Console.WriteLine(edad);
+            }
+
+            public void isMayorEdad()
+            {
+                if (edad >= 18)
+                {
+                    Console.Write("Es mayor de edad");
+                }
+                else
+                {
+                    Console.Write("No es mayor de edad");
+                }
+                Console.ReadKey();
+            }
+        }
+
+            static void Main(string[] args)
         {
             Persona persona = new Persona();
             persona.instanciar();
