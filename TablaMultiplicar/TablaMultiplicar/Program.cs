4ddf1df07498a672d1d97ddf7f178985eda13a37
SilverioUserSvrDev <SilverioUserSvrDev@WINDEV2204EVAL>
2022-06-14 16:49:44 +0200

    codificación TablaMultiplicar

---
+++ TablaMultiplicar/TablaMultiplicar/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTablaMultiplicar
+{
+    internal class TablaMultiplicar
+    {
+        public void CargarValor()
+        {
+            int valor;
+            string linea;
+            do
+            {
+                Console.Write("Ingrese un valor (-1 para finalizar):");
+                linea = Console.ReadLine();
+                valor = int.Parse(linea);
+                if (valor != -1)
+                {
+                    Calcular(valor);
+                }
+            } while (valor != -1);
+        }
+
+        public void Calcular(int v)
+        {
+            for (int f = v; f <= v * 10; f = f + v)
+            {
+                Console.Write(f + "-");
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            TablaMultiplicar tm = new TablaMultiplicar();
+            tm.CargarValor();
+        }
+    }
+}
